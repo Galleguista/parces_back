c08e4e7e33e831b852b03a825e9a6a2ad7e6718b
TI Desarrollador 3 <ti_desarrollo3@administradors-MacBook-Air.local>
2024-08-28 23:22:36 -0500

    please

--- src/admin/recurso/recurso.controller.ts
+++ src/admin/recurso/recurso.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Post, Body, Get, Param, Delete, UseGuards, UploadedFile, UseInterceptors } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { Controller, Post, Body, Get, Param, Delete, UseGuards, UploadedFile, UseInterceptors, UploadedFiles } from '@nestjs/common';
+import { FileFieldsInterceptor, FileInterceptor } from '@nestjs/platform-express';
 import { CreateRecursoDto } from './dto/create-recurso.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { RecursosService } from './recurso.service';
@@ -14,18 +14,27 @@ export class RecursosController {
 
   @UseGuards(JwtAuthGuard)
   @Post('create')
-  @UseInterceptors(FileInterceptor('file', multerConfig(['application/pdf', 'image/jpeg', 'image/png'])))
-  async createRecurso(@UploadedFile() file: Express.Multer.File, @Body() createRecursoDto: CreateRecursoDto) {
-    let imagen_url: string | null = null;
-    let pdf_url: string | null = null;
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'imagen', maxCount: 1 },
+        { name: 'pdf', maxCount: 1 },
+      ],
+      multerConfig(['image/jpeg', 'image/png', 'application/pdf'])
+    )
+  )
+  async createRecurso(@UploadedFiles() files, @Body() createRecursoDto: CreateRecursoDto) {
+    const imagen = files?.imagen?.[0];
+    const pdf = files?.pdf?.[0];
 
-    if (file.mimetype.includes('image')) {
-      imagen_url = `/uploads/${file.path.split('/uploads/')[1]}`;
-    } else if (file.mimetype.includes('pdf')) {
-      pdf_url = `/uploads/${file.path.split('/uploads/')[1]}`;
-    }
+    const imagen_url = imagen ? `/uploads/${imagen.filename}` : null;
+    const pdf_url = pdf ? `/uploads/${pdf.filename}` : null;
 
-    return this.recursosService.createRecurso({ ...createRecursoDto, imagen_url, pdf_url });
+    return this.recursosService.createRecurso({
+      ...createRecursoDto,
+      imagen_url,
+      pdf_url,
+    });
   }
 
   @UseGuards(JwtAuthGuard)
