ec8a17357ab610d682ffbfdd08f956add396a9d4
TI Desarrollador 3 <ti_desarrollo3@administradors-MacBook-Air.local>
2024-08-28 23:10:58 -0500

    test resousrces

--- src/admin/recurso/recurso.controller.ts
+++ src/admin/recurso/recurso.controller.ts
@@ -4,22 +4,34 @@ import { CreateRecursoDto } from './dto/create-recurso.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { multerConfig } from 'src/multer.config';
 import { RecursosService } from './recurso.service';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('recursos')
 export class RecursosController {
   constructor(private readonly recursosService: RecursosService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Post('create')
-  @UseInterceptors(FileInterceptor('pdf', multerConfig))
+  @UseInterceptors(FileInterceptor('pdf', {
+    storage: diskStorage({
+      destination: './uploads/pdf',
+      filename: (req, file, callback) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        const ext = extname(file.originalname);
+        callback(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
+      },
+    }),
+  }))
   async createRecurso(@UploadedFile() pdf: Express.Multer.File, @Body() createRecursoDto: CreateRecursoDto) {
-    const pdf_url = pdf ? `uploads/recursos/${pdf.filename}` : null;
+    console.log('PDF:', pdf);
+    console.log('DTO:', createRecursoDto);
+    const pdf_url = pdf ? `/uploads/pdf/${pdf.filename}` : null;
     if (!pdf_url) {
       throw new Error('PDF file is required');
     }
     return this.recursosService.createRecurso({ ...createRecursoDto, pdf_url });
   }
-
+  
   @UseGuards(JwtAuthGuard)
   @Get()
   async getAllRecursos() {
