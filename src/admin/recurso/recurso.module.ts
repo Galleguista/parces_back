27315e913c7562d1285a7453bd8281c82592473f
Juanito Dev <ykys1xd@gmail.com>
2024-10-21 01:28:01 -0500

    fix files provider error

--- src/admin/recurso/recurso.module.ts
+++ src/admin/recurso/recurso.module.ts
@@ -4,15 +4,15 @@ import { RecursosService } from './recurso.service';
 import { RecursosController } from './recurso.controller';
 import { Recurso } from './entities/recurso.entity';
 import { MulterModule } from '@nestjs/platform-express';
+import { FilesModule } from 'src/system/files/files.module';
+import { FilesService } from 'src/system/files/files.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Recurso]),
-    MulterModule.register({
-      dest: './uploads',
-    }),
+    FilesModule
   ],
-  providers: [RecursosService],
+  providers: [RecursosService, FilesService],
   controllers: [RecursosController],
 })
 export class RecursosModule {}
