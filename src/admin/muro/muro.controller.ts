314db9ecda5653281e2382b9fba86b0bd93547ca
Juanito Dev <ykys1xd@gmail.com>
2024-10-21 02:48:49 -0500

    dsa

--- src/admin/muro/muro.controller.ts
+++ src/admin/muro/muro.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Post, Body, Get, Param, UseGuards, UseInterceptors, UploadedFile, Request } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Get, UseInterceptors, UploadedFile, Request, Param, Res } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { multerConfig } from 'src/multer.config';
 import { MuroService } from './muro.service';
 import { CreatePublicacionDto } from './dto/create-publicacion.dto';
-import { FileInterceptor } from '@nestjs/platform-express';
 import { FilesService } from 'src/system/files/files.service';
-import { multerConfig } from 'src/multer.config';
+import { Response } from 'express';
 
 @Controller('muro')
 export class MuroController {
@@ -15,19 +16,45 @@ export class MuroController {
 
   @UseGuards(JwtAuthGuard)
   @Post('create')
-  @UseInterceptors(FileInterceptor('imagen', multerConfig()))
+  @UseInterceptors(FileInterceptor('imagen', multerConfig())) 
   async createPublicacion(
+    @UploadedFile() file: Express.Multer.File, 
     @Body() createPublicacionDto: CreatePublicacionDto,
-    @UploadedFile() imagen: Express.Multer.File,
     @Request() req: any
   ) {
-    const usuarioId = req.user.usuario_id;
-    return this.muroService.createPublicacion(createPublicacionDto, usuarioId, imagen);
+    try {
+      let imagenUrl = '';
+
+      if (file) {
+        const uploadResult = await this.filesService.handleFileUpload(file, req);
+        imagenUrl = uploadResult.relativePath; 
+        createPublicacionDto.imagen_url = imagenUrl;
+      }
+
+      const newPublicacion = await this.muroService.createPublicacion(createPublicacionDto, req.user.usuario_id);
+      return {
+        success: true,
+        message: 'Publicación creada correctamente.',
+        newPublicacion,
+      };
+    } catch (error) {
+      console.error('Error creando la publicación:', error);
+      return {
+        success: false,
+        message: 'Error interno del servidor. Intente nuevamente.',
+      };
+    }
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('publicaciones')
-  async findAllPublicaciones() {
-    return this.muroService.findAllPublicaciones();
+  async getAllPublicaciones() {
+    const publicaciones = await this.muroService.findAllPublicaciones();
+    return publicaciones.map(publicacion => {
+      if (publicacion.imagen_url) {
+        publicacion.imagen_url = this.filesService.getFileUrl(publicacion.imagen_url); // Obtener la URL pública
+      }
+      return publicacion;
+    });
   }
 }
